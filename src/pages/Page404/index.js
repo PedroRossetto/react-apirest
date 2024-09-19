49a5d9ebfc17ca47882a77980b5e0ad7c61bdd65
Pedro Arthur Rossetto <pedro.rossetto@LTUPAN.INTRANET>
2024-09-19 12:12:33 -0300

    Routes

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Title } from './styled';
 import { Container } from '../../styles/GlobalStyles';
 
 export default function Page404() {
   return (
     <Container>
-      <Title isRed={false}>
-        <h1>Infelizmente está página ainda não existe</h1>
-      </Title>
+      <h1>Infelizmente está página ainda não existe</h1>
     </Container>
   );
 }
