20162d400ee562521bedea7df1d2aa5541213617
Pedro Arthur Rossetto <pedro.rossetto@LTUPAN.INTRANET>
2024-09-19 11:51:39 -0300

    Github pages (React) and history init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Header />
       <Routes />
       <GlobalStyles />
-    </BrowserRouter>
+    </Router>
   );
 }
 
